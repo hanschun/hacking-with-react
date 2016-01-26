fabcd70d6a7769297ece5e197e9651312d848773
Hans Chun <hanschun@gmail.com>
2016-01-25 22:13:24 -0800

    updating files to match tutorial Ch1-16

---
+++ src/pages/Detail.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ajax from 'superagent';
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { commits: [] };
+  }
+  componentWillMount() {
+    ajax.get('https://api.github.com/repos/facebook/react/commits')
+        .end((error, response) => {
+          if (!error && response) {
+            console.dir(response.body);
+            this.setState({ commits: response.body });
+          } else {
+            console.log('There was an error fetching from GitHub', error)
+          }
+        }
+      );
+  }
+
+  render() {
+    return (<div>
+        {this.state.commits.map((commit, index) => {
+          const author = commit.author ? commit.author.login : 'Anonymous';
+
+          return (<p key={index}>
+            <strong>{author}</strong>:
+            <a href={commit.html_url}>{commit.commit.message}</a>.
+            </p>);
+        })}
+      </div>);
+  }
+}
+
+export default Detail;
